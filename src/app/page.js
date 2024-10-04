e5e776c7f92f51497579a5d468b0f0c0c542deb9
MashalTech-rgb <mashalpugc@gmail.com>
2024-10-04 00:55:51 -0700

    comit

--- src/app/page.js
+++ src/app/page.js
@@ -4,18 +4,39 @@ import React, { useState } from 'react';
 const Page = () => {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
-  const [MainTask, setMainTask]=useState("")
+  const [MainTask, setMainTask] = useState([]);
 
   const submitHandling = (e) => {
     e.preventDefault();
-setMainTask([...MainTask,{title,desc}]);
+    setMainTask([...MainTask, { title, desc }]);
     setTitle("");
     setDesc("");
   };
-const renderTask=<h1>No task Available</h1>
+
+  const deleteHandler = (index) => {
+    let copyTask = [...MainTask];
+    copyTask.splice(index, 1);
+    setMainTask(copyTask);
+  };
+
+  let renderTask = <h2 className='heading2'>No Task Available</h2>;
+  if (MainTask.length > 0) {
+    renderTask = MainTask.map((t, i) => {
+      return (
+        <li key={i}>
+          <div className='render'>
+            <h5 className='heading5'>{t.title}</h5>
+            <h5 className='heading5'>{t.desc}</h5>
+            <button className='button ' onClick={() => deleteHandler(i)}>Delete</button>
+          </div>
+        </li>
+      );
+    });
+  }
+
   return (
     <>
-      <h1 className='heading'>My To-Do-App</h1>
+      <h1 className='heading1'>My To-Do-App</h1>
       <form onSubmit={submitHandling}>
         <input 
           type='text' 
@@ -23,7 +44,7 @@ const renderTask=<h1>No task Available</h1>
           placeholder='Enter Title Here'
           value={title} 
           onChange={(e) => {
-            console.log("Title input:", e.target.value); // Log the input
+            console.log("Title input:", e.target.value);
             setTitle(e.target.value);
           }}
         />
@@ -33,14 +54,16 @@ const renderTask=<h1>No task Available</h1>
           placeholder='Enter Description Here'
           value={desc} 
           onChange={(e) => {
-            console.log("Description input:", e.target.value); // Log the input
+            console.log("Description input:", e.target.value);
             setDesc(e.target.value);
           }}
         />
-        <button className='btn'>Add Task</button>
+        <button className='btn'>Click here to Add Task</button>
       </form>
       <div className='tasks'>
-        {renderTask}
+        <ul>
+          {renderTask}
+        </ul>
       </div>
     </>
   );
